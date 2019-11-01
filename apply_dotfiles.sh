d4b081cfb4f99a8e96e9cf1934d0a7cbc17bd921
Xerzz <chaper122@gmail.com>
2019-11-01 16:19:02 +0100

    added ufetch

--- apply_dotfiles.sh
+++ apply_dotfiles.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+cat .bashrc_addons >> ~/.bashrc
 cat .bash_aliases >> ~/.bash_aliases
 cat .bash_functions >> ~/.bash_functions
 cat .inputrc >> ~/.inputrc
 cat .vimrc >> ~/.vimrc
+
+cp ufetch.sh ~/.ufetch.sh
 
 bash
