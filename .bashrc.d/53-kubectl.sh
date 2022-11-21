70126141acfbd15886cbf0910c7b373488360d4a
a-was <adam.wasilewski@unix.com.pl>
2022-11-21 14:44:57 +0100

    Create kubecontext-from-ssh function

--- .bashrc.d/53-kubectl.sh
+++ .bashrc.d/53-kubectl.sh
@@ -8,10 +8,10 @@ function kubecontext-from-file() {
         return 1
     fi
 
-    NAME="$1"
-    FILE="$2"
+    local NAME="$1"
+    local FILE="$2"
 
-    TEMP_FILE="$HOME/.kube/tmp"
+    local TEMP_FILE="$HOME/.kube/tmp"
     KUBECONFIG="$KUBECONFIG:$TEMP_FILE"
 
     cp "$FILE" "$TEMP_FILE"
@@ -21,5 +21,32 @@ function kubecontext-from-file() {
 
     kubectl config view --raw > "$HOME/.kube/config"
 
+    rm "$TEMP_FILE"
+}
+
+function kubecontext-from-ssh() {
+    if [[ -z $1 || -z $2 ]]; then
+        echo "Usage: $0 [context name] [ssh target]"
+        return 1
+    fi
+
+    local NAME="$1"
+    local TARGET="$2"
+
+    local IP="ip route get 1.1.1.1 | head -1 | cut -d' ' -f7"
+    local CONFIG="kubectl config view --minify --flatten"
+    local HOST="cat /etc/hostname"
+
+    local SED1='sed s/127.0.0.1/$('${IP}')/'
+    local SED2='sed s/default/$('${HOST}')/'
+
+    local CMD=${CONFIG}' | '${SED1}' | '${SED2}
+
+    # execute
+    local TEMP_FILE="$(mktemp)"
+    ssh "$TARGET" "$CMD" > "$TEMP_FILE"
+    chmod 600 "$TEMP_FILE"
+
+    kubecontext-from-file "$NAME" "$TEMP_FILE"
     rm "$TEMP_FILE"
 }
